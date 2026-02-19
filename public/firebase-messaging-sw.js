b4024945f7a8f9df7afb82ee8d9f7c65ed85f2e8
alecxander567 <alecxanderespaldon21@gmail.com>
2026-02-20 02:17:22 +0800

    Commit

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 importScripts(
-  "https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js",
+  "https://www.gstatic.com/firebasejs/12.9.0/firebase-app-compat.js",
 );
 importScripts(
-  "https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js",
+  "https://www.gstatic.com/firebasejs/12.9.0/firebase-messaging-compat.js",
 );
 
 firebase.initializeApp({
