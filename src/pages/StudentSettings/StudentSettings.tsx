ccd495703176a9118c60fdff802c487508a370ac
alecxander567 <alecxanderespaldon21@gmail.com>
2026-02-19 11:21:16 +0800

    fixed profile

--- src/pages/StudentSettings/StudentSettings.tsx
+++ src/pages/StudentSettings/StudentSettings.tsx
@@ -433,7 +433,7 @@ function StudentSettings() {
                       style={{ position: "relative", display: "inline-block" }}>
                       {profile.profile_image ?
                         <img
-                          src={`${import.meta.env.VITE_API_URL}/${profile.profile_image}`}
+                          src={`${import.meta.env.VITE_API_URL}/${profile.profile_image.replace(/^\//, "")}`}
                           alt="Profile"
                           style={{
                             width: "100px",
